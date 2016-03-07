f17f3b87c53c4cad779e650a171fa7cff61f11a5
Jack Schuss <jackschuss@gmail.com>
2016-03-07 13:49:07 -0500

    verified working curl request for api/users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,8 +2,17 @@ class Api::UsersController < ApiController
   before_action :authenticated?
 
   def index
+    return permission_denied_error unless conditions_met
+
+    users = User.all
 
     render json: users, each_serializer: UserSerializer
+  end
+
+  private
+
+  def conditions_met
+    true
   end
 
 end
