21b209842fe1d8c47341d9b5f99f60cf4afe97f6
Jack Schuss <jackschuss@gmail.com>
2016-03-07 16:51:59 -0500

    Add destroy actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,6 +20,19 @@ class Api::UsersController < ApiController
     end
   end
 
+  def destroy
+    
+    begin
+      user = User.find(params[:id])
+      user.destroy
+      render json: {}, status: :no_content
+
+    rescue ActiveRecord::RecordNotFound
+      render json: {}, status: :not_found
+    end
+
+  end
+
   private
 
   def conditions_met
