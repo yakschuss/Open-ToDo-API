84694463a5368082faa600009ea73bae07e9b015
Jack Schuss <jackschuss@gmail.com>
2016-03-07 18:14:36 -0500

    tweak relationships, foreign_keys

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
+    belongs_to :list
+    validates :list_id, presence: true
     validates :description, presence: true
 end
--- db/migrate/20160306155502_create_lists.rb
+++ db/migrate/20160306155502_create_lists.rb
@@ -2,7 +2,6 @@ class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
       t.integer :user_id
-      t.integer :item_id
       t.string :name
       t.string :permissions
       t.timestamps null: false
